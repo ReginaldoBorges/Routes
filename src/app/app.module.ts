bae13366c022ec9a3b39812b013cc343114a0c04
reginaldo borges <reginaldo.borges07@gmail.com>
2023-12-04 08:23:13 -0300

    Secao_12_74 Novos components e criação de rotas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// Pages
+import { HomeComponent } from './pages/home/home.component';
+import { SobreComponent } from './pages/sobre/sobre.component';
+import { PageErrorComponent } from './pages/page-error/page-error.component';
+
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    SobreComponent,
+    PageErrorComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
